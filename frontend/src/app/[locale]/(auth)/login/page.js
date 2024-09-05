7e5f79f796f6acac6f22ffb3269c4309499c5987
ifirmawan <firmawaneiwan@gmail.com>
2024-09-05 10:14:28 +0700

    [#8] Implement successVerified

--- frontend/src/app/[locale]/(auth)/login/page.js
+++ frontend/src/app/[locale]/(auth)/login/page.js
@@ -1,10 +1,23 @@
+import { redirect } from "@/routing";
+import { Alert } from "antd";
+import { useTranslations } from "next-intl";
+
 export const metadata = {
   title: "Login | PARTOS-PAT",
   description: "Make power dynamics more visible",
 };
 
-const LoginPage = () => {
-  return <h1>Login page</h1>;
+const LoginPage = ({ searchParams }) => {
+  const t = useTranslations("Register");
+  const { verified } = searchParams;
+  return (
+    <>
+      {verified && (
+        <Alert type="success" message={t("successVerified")} closable />
+      )}
+      <h1>Login page</h1>
+    </>
+  );
 };
 
 export default LoginPage;
