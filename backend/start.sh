7bf097b188ac0d9dd2a74cb48d273a2486b43bac
dedenbangkit <mail@dedenbangkit.com>
2024-09-05 14:07:59 +0700

    [#1] Add migrate to start sh

--- backend/start.sh
+++ backend/start.sh
@@ -3,4 +3,5 @@ set -eu
 
 pip -q install --upgrade pip
 pip -q install --cache-dir=.pip -r requirements.txt
-python manage.py runserver 0.0.0.0:8000
+./manage.py migrate
+./manage.py runserver 0.0.0.0:8000
