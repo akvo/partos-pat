493d46dcc20409020a2d015698a0240542300d23
ifirmawan <firmawaneiwan@gmail.com>
2024-09-06 16:41:17 +0700

    [#13] Create my profile API test cases

---
+++ backend/api/v1/v1_users/tests/tests_users_me_endpoint.py
@@ -0,0 +1,50 @@
+from django.test import TestCase
+from api.v1.v1_users.models import SystemUser
+from api.v1.v1_users.tests.mixins import ProfileTestHelperMixin
+
+
+class MyProfileTestCase(TestCase, ProfileTestHelperMixin):
+    def setUp(self):
+        email = "john@test.com"
+        password = "Open1234"
+        self.user = SystemUser.objects.create_user(
+            full_name="John Doe",
+            email=email,
+            gender=1,
+            account_purpose=1,
+            country="EN",
+            password=password,
+        )
+        self.reset_db_sequence(SystemUser)
+        self.token = self.get_auth_token(
+            email=email,
+            password=password
+        )
+
+    def test_successfully_get_my_account(self):
+        req = self.client.get(
+            "/api/v1/users/me",
+            content_type="application/json",
+            HTTP_AUTHORIZATION=f"Bearer {self.token}"
+        )
+        self.assertTrue(req.status_code, 200)
+        res = req.json()
+        self.assertEqual(
+            list(res),
+            ["id", "full_name", "email", "gender", "country"]
+        )
+        self.assertEqual(res["full_name"], "John Doe")
+
+    def test_my_deleted_account(self):
+        self.user.soft_delete()
+        req = self.client.get(
+            "/api/v1/users/me",
+            content_type="application/json",
+            HTTP_AUTHORIZATION=f"Bearer {self.token}"
+        )
+        self.assertTrue(req.status_code, 401)
+        res = req.json()
+        self.assertEqual(
+            res,
+            {"detail": "User not found", "code": "user_not_found"}
+        )
