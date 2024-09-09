954731953d1c50ed09a2c0abd72730c6d61484d5
ifirmawan <firmawaneiwan@gmail.com>
2024-09-09 20:39:43 +0700

    [#13] Export all components from Buttons

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,4 +1,4 @@
-export { default as RegisterForm } from "./Forms/RegisterForm";
-export { default as SubmitButton } from "./SubmitButton";
-export { default as LoginForm } from "./Forms/LoginForm";
 export * from "./PasswordInput";
+export * from "./Buttons";
+export { default as RegisterForm } from "./Forms/RegisterForm";
+export { default as LoginForm } from "./Forms/LoginForm";
