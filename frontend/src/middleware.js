26c2538d279891a9a9b507c332447a5320d99b20
ifirmawan <firmawaneiwan@gmail.com>
2024-09-09 20:37:06 +0700

    [#13] Implements OptimisticCheck in middleware

--- frontend/src/middleware.js
+++ frontend/src/middleware.js
@@ -1,6 +1,11 @@
 import createMiddleware from "next-intl/middleware";
+import { OptimisticCheck } from "./lib/auth";
 
 export default async function middleware(request) {
+  const [, locale, ...segments] = request.nextUrl.pathname.split("/");
+  const pathName = segments.join("/");
+  OptimisticCheck(locale, pathName, request);
+
   const handleI18nRouting = createMiddleware({
     locales: ["en", "fr"],
     defaultLocale: "en",
