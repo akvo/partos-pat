a8f8f97541cc533a6cd8a82b286f78d6589b93b1
ifirmawan <firmawaneiwan@gmail.com>
2024-09-09 20:41:32 +0700

    [#13] Implement LogoutButton in profile page

---
+++ frontend/src/app/[locale]/dashboard/profile/page.js
@@ -0,0 +1,13 @@
+import { LogoutButton } from "@/components";
+
+const ProfilePage = () => {
+  return (
+    <div class="w-full p-4 space-y-4">
+      <div className="w-full flex items-center justify-end">
+        <LogoutButton />
+      </div>
+    </div>
+  );
+};
+
+export default ProfilePage;
