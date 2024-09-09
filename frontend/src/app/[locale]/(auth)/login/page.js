32c1d1eb304df2034b05004489a467968c2869fe
ifirmawan <firmawaneiwan@gmail.com>
2024-09-09 10:18:19 +0700

    [#13] Implements login form in LoginPage

--- frontend/src/app/[locale]/(auth)/login/page.js
+++ frontend/src/app/[locale]/(auth)/login/page.js
@@ -1,6 +1,8 @@
-import { redirect } from "@/routing";
-import { Alert } from "antd";
 import { useTranslations } from "next-intl";
+import { PartosLogo } from "@/components/Icons";
+import { Link } from "@/routing";
+import { LoginForm } from "@/components";
+import { Alert, Button } from "antd";
 
 export const metadata = {
   title: "Login | PARTOS-PAT",
@@ -8,15 +10,28 @@ export const metadata = {
 };
 
 const LoginPage = ({ searchParams }) => {
-  const t = useTranslations("Register");
+  const t = useTranslations("Login");
+  const tr = useTranslations("Register");
   const { verified } = searchParams;
   return (
-    <>
+    <div className="w-full h-auto space-y-6 mb-8">
       {verified && (
-        <Alert type="success" message={t("successVerified")} closable />
+        <Alert type="success" message={tr("successVerified")} closable />
       )}
-      <h1>Login page</h1>
-    </>
+      <div className="w-full flex items-center justify-center pt-4 text-dark-10">
+        <Link href="/">
+          <PartosLogo width={125} height={125} />
+        </Link>
+      </div>
+      <LoginForm />
+      <div className="mt-4">
+        <Link href="/register">
+          <Button htmlType="button" ghost block>
+            {t("createAccount")}
+          </Button>
+        </Link>
+      </div>
+    </div>
   );
 };
 
