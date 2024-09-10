0fd2883c3a7637cd1a96194fa2bcfc3c584b021c
ifirmawan <firmawaneiwan@gmail.com>
2024-09-10 09:42:55 +0700

    [#13] Redirect to the login page when the session is not valid

--- frontend/src/middleware.js
+++ frontend/src/middleware.js
@@ -4,13 +4,23 @@ import { OptimisticCheck } from "./lib/auth";
 export default async function middleware(request) {
   const [, locale, ...segments] = request.nextUrl.pathname.split("/");
   const pathName = segments.join("/");
-  OptimisticCheck(locale, pathName, request);
+  const isValid = await OptimisticCheck(locale, pathName, request);
 
   const handleI18nRouting = createMiddleware({
     locales: ["en", "fr"],
     defaultLocale: "en",
   });
   const response = handleI18nRouting(request);
+
+  if (!isValid) {
+    response.cookies.set({
+      name: "currentUser",
+      value: "",
+      httpOnly: true,
+      expires: new Date(0),
+    });
+    request.nextUrl.pathname = `/${locale}/login`;
+  }
   return response;
 }
 
