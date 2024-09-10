631fd08dc654037a1a4836c39f73a6243cf33975
ifirmawan <firmawaneiwan@gmail.com>
2024-09-10 15:48:30 +0700

    [#13] Refactoring API library w/ method string param

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,40 +1,24 @@
 "use server";
 import { getSession } from "./auth";
 
-const fetchApi = (url, props) =>
+export const api = (method, url, payload = {}) =>
   new Promise(async (resolve, reject) => {
     const { token: authToken } = await getSession();
-    fetch(`${process.env.WEBDOMAIN}/api/v1/${url}`, {
-      ...props,
-      headers: {
-        "Content-Type": "application/json",
-        ...props?.headers,
-        Authorization: `Bearer ${authToken}`,
-      },
+    fetch(`${process.env.WEBDOMAIN}/api/v1${url}`, {
+      method,
+      headers:
+        typeof payload === "object" && Object.keys(payload).length
+          ? {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${authToken}`,
+              body: JSON.stringify(payload),
+            }
+          : {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${authToken}`,
+            },
     })
       .then((res) => res.json())
       .then((res) => resolve(res))
       .catch((err) => reject(err));
-  });
-
-export const apiGet = (url) =>
-  fetchApi(url, {
-    method: "GET",
-  });
-
-export const apiPost = (url, payload) =>
-  fetchApi(url, {
-    method: "POST",
-    body: JSON.stringify(payload),
-  });
-
-export const apiPut = (url, payload) =>
-  fetchApi(url, {
-    method: "PUT",
-    body: JSON.stringify(payload),
-  });
-
-export const apiDelete = (url) =>
-  fetchApi(url, {
-    method: "DELETE",
   });
