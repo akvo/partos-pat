23f8fabadfab51c26f235ba6f39e24b73a98cac8
ifirmawan <firmawaneiwan@gmail.com>
2024-09-10 14:36:30 +0700

    [#13] Add loading state in LogoutButton

--- frontend/src/components/Buttons/LogoutButton.js
+++ frontend/src/components/Buttons/LogoutButton.js
@@ -1,20 +1,23 @@
 "use client";
 
+import { useState } from "react";
 import { useRouter } from "next/navigation";
 import { Button } from "antd";
 import { signOut } from "@/lib/auth";
 import { useTranslations } from "next-intl";
 
 const LogoutButton = () => {
+  const [loading, setLoading] = useState(false);
   const router = useRouter();
   const t = useTranslations("common");
 
   const onLogout = async () => {
+    setLoading(true);
     await signOut();
     router.push("/");
   };
   return (
-    <Button htmlType="button" onClick={onLogout} ghost>
+    <Button htmlType="button" onClick={onLogout} loading={loading} ghost>
       {t("logout")}
     </Button>
   );
