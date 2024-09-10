f31cb3dee204c6c610287daa4a9fea10eff5cce0
ifirmawan <firmawaneiwan@gmail.com>
2024-09-10 09:45:16 +0700

    [#13] Add forgot password link in LoginForm

--- frontend/src/components/Forms/LoginForm.js
+++ frontend/src/components/Forms/LoginForm.js
@@ -1,10 +1,10 @@
 "use client";
 
 import { useState } from "react";
-import { Form, Input, message } from "antd";
+import { Button, Form, Input, message } from "antd";
 import { useTranslations } from "next-intl";
-import { Envelope } from "@/components/Icons";
-import { useRouter } from "@/routing";
+import { Envelope, LockIcon } from "@/components/Icons";
+import { Link, useRouter } from "@/routing";
 import SubmitButton from "../Buttons/SubmitButton";
 import { PasswordInput } from "../PasswordInput";
 import { signIn } from "@/lib/auth";
@@ -24,10 +24,10 @@ const LoginForm = () => {
     setSubmitting(true);
     try {
       const { status, message: errorKey } = await signIn(values);
-      setSubmitting(false);
       if (status === 200) {
         router.push("/dashboard");
       } else {
+        setSubmitting(false);
         message.error(tr(errorKey));
       }
     } catch (error) {
@@ -67,6 +67,13 @@ const LoginForm = () => {
         placeholder={tc("password")}
         className="min-h-10"
       />
+      <div className="w-full text-right mb-6">
+        <Link href="/forgot-password">
+          <Button type="link" icon={<LockIcon />}>
+            {t("forgotLink")}
+          </Button>
+        </Link>
+      </div>
       <SubmitButton form={form} loading={submitting} block>
         {t("loginText")}
       </SubmitButton>
