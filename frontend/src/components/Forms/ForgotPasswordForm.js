e890b3ae3fb2968f406202be16520f742d8f5336
dedenbangkit <mail@dedenbangkit.com>
2024-09-10 21:14:56 +0700

    [#16] Frontend page for forgot password

---
+++ frontend/src/components/Forms/ForgotPasswordForm.js
@@ -0,0 +1,81 @@
+"use client";
+
+import { useState } from "react";
+import { Button, Form, Input, message } from "antd";
+import { useTranslations } from "next-intl";
+import { Envelope, LockIcon } from "@/components/Icons";
+import SubmitButton from "../Buttons/SubmitButton";
+
+const { useForm } = Form;
+
+const ForgotPasswordForm = () => {
+  const [loading, setLoading] = useState(false);
+  const [success, setSuccess] = useState(false);
+  const [form] = useForm();
+
+  const t = useTranslations("ForgotPassword");
+  const tc = useTranslations("common");
+  const tr = useTranslations("Error");
+
+  const onFinish = async (values) => {
+    console.log(values);
+    setLoading(true);
+    try {
+      const response = await fetch("/api/v1/users/forgot-password", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(values),
+      });
+      if (response.ok) {
+        setSuccess(true);
+        setLoading(false);
+      } else {
+        const error = await response.json();
+        throw new Error(error.message);
+        setLoading(false);
+      }
+    } catch (error) {
+      const errorKey = error.message.replace(/^Error:\s*/, "");
+      message.error(tr(errorKey));
+      setLoading(false);
+    }
+  };
+
+  if (success) {
+    return (
+      <div className="text-center">
+        <h2 className="text-2xl font-semibold mt-4">{t("emailSent")}</h2>
+        <p className="text-gray-500 mt-2">{t("emailSentDescription")}</p>
+      </div>
+    );
+  }
+
+  return (
+    <Form name="forgot-password" form={form} onFinish={onFinish}>
+      <Form.Item
+        name="email"
+        rules={[
+          {
+            required: true,
+            type: "email",
+            message: tc("emailRequired"),
+          },
+        ]}
+      >
+        <Input
+          placeholder={tc("email")}
+          type="email"
+          prefix={<Envelope />}
+          variant="borderless"
+        />
+      </Form.Item>
+      <SubmitButton form={form} loading={loading} block>
+        {t("sendLink")}
+      </SubmitButton>
+    </Form>
+  );
+};
+
+export default ForgotPasswordForm;
--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -2,3 +2,4 @@ export * from "./PasswordInput";
 export * from "./Buttons";
 export { default as RegisterForm } from "./Forms/RegisterForm";
 export { default as LoginForm } from "./Forms/LoginForm";
+export { default as ForgotPasswordForm } from "./Forms/ForgotPasswordForm";
