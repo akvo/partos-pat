15cb25606a0b473a9825668163104a92c290d845
ifirmawan <firmawaneiwan@gmail.com>
2024-09-12 12:11:52 +0700

    [#15] Create new component: HorizontalDivider

---
+++ frontend/src/components/HorizontalDivider.js
@@ -0,0 +1,7 @@
+const HorizontalDivider = ({ children }) => (
+  <div className="w-fit py-4">
+    <div className="flex divide-x-2 divide-solid">{children}</div>
+  </div>
+);
+
+export default HorizontalDivider;
