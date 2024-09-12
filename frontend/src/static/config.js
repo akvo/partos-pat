fbb96917c8637597ab0e2fbc2aad038523fa5847
ifirmawan <firmawaneiwan@gmail.com>
2024-09-12 09:01:59 +0700

    [#15] Create new config: DASHBOARD_MENU

--- frontend/src/static/config.js
+++ frontend/src/static/config.js
@@ -1,3 +1,5 @@
+import { DashboardIcon } from "@/components/Icons";
+
 export const GENDER = {
   male: 1,
   female: 2,
@@ -16,3 +18,24 @@ export const PURPOSE_OF_ACCOUNT = {
 export const PARTOS = {
   email: "niels@partos.nl",
 };
+
+export const DASHBOARD_MENU = [
+  {
+    id: 1,
+    name: "dashboard",
+    icon: <DashboardIcon />,
+    url: "/dashboard",
+  },
+  {
+    id: 2,
+    name: "faqs",
+    icon: <DashboardIcon />,
+    url: "/faqs",
+  },
+  {
+    id: 3,
+    name: "resources",
+    icon: <DashboardIcon />,
+    url: "/pat-resources",
+  },
+];
