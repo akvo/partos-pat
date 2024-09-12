b18e8f06ebeb6d3894684cbd3e8ded7774ecd875
ifirmawan <firmawaneiwan@gmail.com>
2024-09-12 09:05:27 +0700

    [#15] Update exported components

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,5 +1,9 @@
 export * from "./PasswordInput";
 export * from "./Buttons";
+export * from "./Modals";
 export { default as RegisterForm } from "./Forms/RegisterForm";
 export { default as LoginForm } from "./Forms/LoginForm";
 export { default as ForgotPasswordForm } from "./Forms/ForgotPasswordForm";
+export { default as ClosedSessionList } from "./ClosedSessionList";
+export { default as ActiveSessionList } from "./ActiveSessionList";
+export { default as DashboardMenu } from "./DashboardMenu";
