1a75b94bbc1521f60636a135142874ed30e7dee5
ifirmawan <firmawaneiwan@gmail.com>
2024-09-12 20:27:14 +0700

    [#20] Create constants for SectorTypes

---
+++ backend/api/v1/v1_sessions/constants.py
@@ -0,0 +1,20 @@
+class SectorTypes:
+    sector1 = 1
+    sector2 = 2
+    sector3 = 3
+    sector4 = 4
+    sector5 = 5
+    sector6 = 6
+    sector7 = 7
+    sector8 = 8
+
+    FieldStr = {
+        sector1: "Agriculture and Food Security",
+        sector2: "Disaster Relief and Humanitarian Aid",
+        sector3: "Economic Development and Poverty Alleviation",
+        sector4: "Education",
+        sector5: "Environmental Conservation",
+        sector6: "Health and Medical Services",
+        sector7: "Human Rights and Social Justice",
+        sector8: "Other"
+    }
