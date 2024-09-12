e40824546d7d562641a31b5d88cc3af9caecfc28
ifirmawan <firmawaneiwan@gmail.com>
2024-09-12 12:20:09 +0700

    [#15] Implements PATLogo & HorizontalDivider in with-navbar template

--- frontend/src/app/[locale]/(with-navbar)/learn-more/page.js
+++ frontend/src/app/[locale]/(with-navbar)/learn-more/page.js
@@ -1,19 +1,18 @@
 import { useTranslations } from "next-intl";
 import { Link } from "@/routing";
+import { HorizontalDivider } from "@/components";
 
 const LearnMorePage = () => {
   const t = useTranslations("Dashboard");
   return (
-    <div className="w-fit lg:w-1/5 py-4">
-      <div className="flex divide-x-2 divide-solid">
-        <div className="pr-3">
-          <Link href="/dashboard">{t("dashboard")}</Link>
-        </div>
-        <div className="px-3">
-          <h1 className="font-bold text-xl">{t("learnMore")}</h1>
-        </div>
+    <HorizontalDivider>
+      <div className="pr-3">
+        <Link href="/dashboard">{t("dashboard")}</Link>
       </div>
-    </div>
+      <div className="px-3">
+        <h1 className="font-bold text-xl">{t("learnMore")}</h1>
+      </div>
+    </HorizontalDivider>
   );
 };
 
--- frontend/src/app/[locale]/(with-navbar)/template.js
+++ frontend/src/app/[locale]/(with-navbar)/template.js
@@ -1,3 +1,4 @@
+import { PATLogo } from "@/components";
 import { QuestionMarkIcon } from "@/components/Icons";
 import { Link } from "@/routing";
 import { Flex } from "antd";
@@ -9,16 +10,11 @@ const WithNavbarTemplate = ({ children }) => {
       <div className="w-full flex items-center bg-light-1 h-24">
         <div className="container mx-auto px-2 md:px-0">
           <Flex className="w-full" justify="space-between" align="center">
-            <div>
-              <Link href="/dashboard">
-                <Image
-                  width={260}
-                  height={55}
-                  alt="Power Awareness Tool Logo"
-                  src="https://placehold.co/260x55"
-                />
-              </Link>
-            </div>
+            <Link href="/dashboard">
+              <div className="text-dark-10">
+                <PATLogo />
+              </div>
+            </Link>
             <Link href="/dashboard">
               <div className="border-2 border-[#3C3C3C] p-2 rounded-full text-dark-10">
                 <QuestionMarkIcon size={24} />
