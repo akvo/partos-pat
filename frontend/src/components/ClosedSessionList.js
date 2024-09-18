09dc21e2ac893cc70a8763088081e3e860ace857
ifirmawan <firmawaneiwan@gmail.com>
2024-09-18 16:19:23 +0700

    [#27] Add title in ClosedSessionList

--- frontend/src/components/ClosedSessionList.js
+++ frontend/src/components/ClosedSessionList.js
@@ -2,34 +2,44 @@
 
 import { Button, Card, Flex, List, Typography } from "antd";
 import { DotsTreeVerticalIcon } from "./Icons";
+import { useTranslations } from "next-intl";
+import { PAT_SESSION } from "@/static/config";
 
 const { Title, Text } = Typography;
 
-const ClosedSessionList = ({ data = [] }) => {
+const ClosedSessionList = ({ data = [], totalClosed = 0 }) => {
+  const t = useTranslations("Dashboard");
   return (
-    <List
-      pagination={{
-        pageSize: 5,
-      }}
-      dataSource={data}
-      renderItem={(item) => (
-        <div className="mb-3">
-          <Card hoverable>
-            <Flex align="center" justify="space-between">
-              <div>
-                <Title level={4}>{item?.session_name}</Title>
-                <Text>{item?.context}</Text>
-              </div>
-              <div>
-                <Button type="link">
-                  <DotsTreeVerticalIcon />
-                </Button>
-              </div>
-            </Flex>
-          </Card>
-        </div>
-      )}
-    />
+    <div className="w-full space-y-4">
+      <h2 className="font-bold text-xl">{t("closedSessions")}</h2>
+      <List
+        pagination={
+          totalClosed > PAT_SESSION.pageSize
+            ? {
+                pageSize: PAT_SESSION.pageSize,
+              }
+            : false
+        }
+        dataSource={data}
+        renderItem={(item) => (
+          <div className="mb-3">
+            <Card hoverable>
+              <Flex align="center" justify="space-between">
+                <div>
+                  <Title level={4}>{item?.session_name}</Title>
+                  <Text>{item?.context}</Text>
+                </div>
+                <div>
+                  <Button type="link">
+                    <DotsTreeVerticalIcon />
+                  </Button>
+                </div>
+              </Flex>
+            </Card>
+          </div>
+        )}
+      />
+    </div>
   );
 };
 
