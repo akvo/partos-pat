62a15ed29cf7a406dfbf38677f1c73bbf648efbb
ifirmawan <firmawaneiwan@gmail.com>
2024-09-19 13:54:51 +0700

    [#29] Change bg-grey to bg-grey-100

--- frontend/src/app/[locale]/(with-navbar)/template.js
+++ frontend/src/app/[locale]/(with-navbar)/template.js
@@ -2,11 +2,10 @@ import { PATLogo } from "@/components";
 import { QuestionMarkIcon } from "@/components/Icons";
 import { Link } from "@/routing";
 import { Flex } from "antd";
-import Image from "next/image";
 
 const WithNavbarTemplate = ({ children }) => {
   return (
-    <div className="w-full max-w-9xl h-screen bg-grey text-base text-dark-10 overflow-y-auto lg:overflow-y-hidden">
+    <div className="w-full max-w-9xl h-screen bg-grey-100 text-base text-dark-10 overflow-y-auto lg:overflow-y-hidden">
       <div className="w-full flex items-center bg-light-1 h-24">
         <div className="container mx-auto px-2 md:px-0">
           <Flex className="w-full" justify="space-between" align="center">
--- frontend/src/app/[locale]/dashboard/template.js
+++ frontend/src/app/[locale]/dashboard/template.js
@@ -3,7 +3,7 @@ import { Link } from "@/routing";
 
 const DashboardTemplate = ({ children }) => {
   return (
-    <div className="w-full max-w-9xl h-screen bg-grey flex flex-col md:flex-row text-base text-dark-10 overflow-y-auto lg:overflow-y-hidden">
+    <div className="w-full max-w-9xl h-screen bg-grey-100 flex flex-col md:flex-row text-base text-dark-10 overflow-y-auto lg:overflow-y-hidden">
       <aside className="w-full h-auto md:w-64 lg:w-72 md:h-screen bg-light-1 shadow-lg flex flex-col justify-between">
         <div className="w-full h-auto lg:h-[calc(100vh-235px)] space-y-8 mt-8">
           <div className="px-4">
