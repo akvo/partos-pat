633e4489fab99bbe4edbd30c8a56f95e3d87d921
ifirmawan <firmawaneiwan@gmail.com>
2024-09-20 13:50:59 +0700

    [#15] Remove support page & change with HelpModal

--- frontend/src/components/DashboardMenu.js
+++ frontend/src/components/DashboardMenu.js
@@ -1,15 +1,17 @@
 "use client";
 
-import { Button, Flex } from "antd";
+import { Button, Flex, Modal } from "antd";
 import { useTranslations } from "next-intl";
 import { DASHBOARD_MENU } from "@/static/config";
 import { useRouter } from "@/routing";
 import { LifebuoyIcon } from "./Icons";
 import { useState } from "react";
 import classNames from "classnames";
+import { HelpModal } from "./Modals";
 
 const DashboardMenu = () => {
   const [activeKey, setActiveKey] = useState(1);
+  const [open, setOpen] = useState(false);
 
   const router = useRouter();
   const t = useTranslations("Dashboard");
@@ -39,13 +41,14 @@ const DashboardMenu = () => {
         <Button
           type="link"
           onClick={() => {
-            router.push("/dashboard/support");
+            setOpen(true);
           }}
           icon={<LifebuoyIcon />}
           size="large"
         >
           {t("support")}
         </Button>
+        <HelpModal {...{ open, setOpen }} />
       </div>
     </Flex>
   );
