e1e54a3c3b30ca09799b5ee055fc8ebf105c38ea
ifirmawan <firmawaneiwan@gmail.com>
2024-09-24 16:44:23 +0700

    [#38] Create WithHeader template in Dashboard route

---
+++ frontend/src/app/[locale]/dashboard/(with-header)/template.js
@@ -0,0 +1,15 @@
+import { BreadCrumb } from "@/components";
+
+const WithHeaderTemplate = ({ children }) => {
+  return (
+    <>
+      <div className="w-full h-44 flex flex-col justify-center bg-dashboard-header-gradient px-10">
+        <div className="w-[420px] h-44 absolute top-0 left-[280px] bg-dashboard-circle bg-no-repeat bg-cover bg-center" />
+        <BreadCrumb />
+      </div>
+      <div className="w-full lg:w-10/12 px-5 py-8">{children}</div>
+    </>
+  );
+};
+
+export default WithHeaderTemplate;
