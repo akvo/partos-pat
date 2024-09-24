7422272cc8b673b5a480f4d68438cc455a9d108d
ifirmawan <firmawaneiwan@gmail.com>
2024-09-24 16:47:08 +0700

    [#38] Fix responsive issue in dashboard templates

--- frontend/src/app/[locale]/(with-navbar)/template.js
+++ frontend/src/app/[locale]/(with-navbar)/template.js
@@ -5,8 +5,9 @@ import { Link } from "@/routing";
 const WithNavbarTemplate = ({ children }) => {
   return (
     <div className="w-full max-w-9xl h-screen bg-grey-100 text-base text-dark-10 overflow-y-auto lg:overflow-y-hidden">
+      <div className="w-1/2 lg:w-2/5 h-screen absolute bottom-0 right-0 bg-dashboard bg-no-repeat bg-contain bg-right-bottom" />
       <div className="w-full flex items-center bg-light-1 h-24">
-        <div className="container mx-auto px-2 md:px-0">
+        <div className="container mx-auto px-4">
           <Flex className="w-full" justify="space-between" align="center">
             <Link href="/dashboard">
               <div className="text-dark-10">
@@ -17,7 +18,7 @@ const WithNavbarTemplate = ({ children }) => {
           </Flex>
         </div>
       </div>
-      <div className="container mx-auto px-2 md:px-0">{children}</div>
+      <div className="container mx-auto px-4">{children}</div>
     </div>
   );
 };
