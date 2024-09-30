12ab70e5236ef6cbd9efc724ad265b6e4b054c25
ifirmawan <firmawaneiwan@gmail.com>
2024-09-30 21:33:16 +0700

    [#48] Update step2 description

--- frontend/src/components/SessionWizard/Step2.js
+++ frontend/src/components/SessionWizard/Step2.js
@@ -1,10 +1,16 @@
 "use client";
 
+import { useTranslations } from "next-intl";
+
 const StepTwo = () => {
+  const t = useTranslations("Session");
   return (
-    <>
-      <h1>Step 2</h1>
-    </>
+    <div className="w-full space-y-6">
+      <div className="w-full space-y-4 whitespace-pre-line">
+        <strong>{t("step2Title")}</strong>
+        <p>{t("step2Desc")}</p>
+      </div>
+    </div>
   );
 };
 export default StepTwo;
