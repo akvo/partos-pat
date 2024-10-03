0a770f3414afe72a0b3c423aff5601abf8c003c8
ifirmawan <firmawaneiwan@gmail.com>
2024-10-03 19:43:49 +0700

    [#60] Fix vertical scroll in dashboard template

--- frontend/src/app/[locale]/dashboard/template.js
+++ frontend/src/app/[locale]/dashboard/template.js
@@ -3,12 +3,14 @@ import { Link } from "@/routing";
 
 const DashboardTemplate = ({ children }) => {
   return (
-    <div className="w-full max-w-9xl h-screen bg-grey-100 flex flex-col md:flex-row text-base text-dark-10 overflow-y-auto lg:overflow-y-hidden">
+    <div className="w-full max-w-9xl h-screen relative bg-grey-100 flex flex-col md:flex-row text-base text-dark-10 overflow-hidden">
       <div className="w-1/2 lg:w-2/5 h-screen absolute bottom-0 right-0 bg-dashboard bg-no-repeat bg-contain bg-right-bottom" />
       <aside className="w-full h-auto md:w-64 lg:w-72 md:h-screen bg-light-1 shadow-lg flex flex-col justify-between">
         <div className="w-full h-auto lg:h-[calc(100vh-235px)] space-y-8 mt-8">
           <div className="px-4">
-            <PATLogo />
+            <Link href="/">
+              <PATLogo />
+            </Link>
           </div>
           <DashboardMenu />
         </div>
@@ -20,7 +22,7 @@ const DashboardTemplate = ({ children }) => {
           </li>
         </ul>
       </aside>
-      <main className="w-full">{children}</main>
+      <main className="w-full h-screen overflow-y-auto">{children}</main>
     </div>
   );
 };
