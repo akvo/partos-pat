7bbe77007620c604a7f25fa868bd902dff100dd9
ifirmawan <firmawaneiwan@gmail.com>
2024-10-04 20:05:36 +0700

    [#64] Add large prop in ProfileAvatar for user profile page

--- frontend/src/components/ProfileAvatar.js
+++ frontend/src/components/ProfileAvatar.js
@@ -2,21 +2,37 @@
 
 import { useUserContext } from "@/context/UserContextProvider";
 import { Avatar, Space } from "antd";
+import classNames from "classnames";
 
-const ProfileAvatar = () => {
+const ProfileAvatar = ({ large = false }) => {
   const userContext = useUserContext();
 
   const { full_name, email } = userContext;
   const [firstName, lastName] = full_name?.split(/\s/g);
 
   return (
-    <Space>
+    <Space size={large ? "large" : "small"}>
       <Avatar
-        size={48}
+        size={large ? 96 : 48}
       >{`${firstName ? firstName[0].toUpperCase() : ""}${lastName ? lastName?.[0]?.toUpperCase() : ""}`}</Avatar>
       <div>
-        <strong>{full_name}</strong>
-        <p className="w-full md:w-48 overflow-x-hidden text-sm text-ellipsis">
+        <strong
+          className={classNames({
+            "text-base": !large,
+            "text-2xl": large,
+          })}
+        >
+          {full_name}
+        </strong>
+        <p
+          className={classNames(
+            "w-full md:w-48 overflow-x-hidden text-ellipsis",
+            {
+              "text-sm": !large,
+              "text-base": large,
+            }
+          )}
+        >
           {email}
         </p>
       </div>
