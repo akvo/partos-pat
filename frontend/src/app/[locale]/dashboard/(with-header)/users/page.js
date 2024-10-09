c69cc62248fdec5ac9a01259dcc5e77936f27a61
ifirmawan <firmawaneiwan@gmail.com>
2024-10-09 15:11:28 +0700

    [#66] Finalizing backend integration in manage users page

--- frontend/src/app/[locale]/dashboard/(with-header)/users/page.js
+++ frontend/src/app/[locale]/dashboard/(with-header)/users/page.js
@@ -1,14 +1,9 @@
-import { Card } from "antd";
 import { UsersTable } from "@/components";
 import { api } from "@/lib";
 
 const UsersPage = async () => {
   const { total, data } = await api("GET", "/admin/users");
-  return (
-    <Card>
-      <UsersTable total={total} initialData={data} />
-    </Card>
-  );
+  return <UsersTable total={total} initialData={data} />;
 };
 
 export default UsersPage;
