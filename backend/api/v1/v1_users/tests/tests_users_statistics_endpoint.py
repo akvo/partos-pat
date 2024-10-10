e05650e410ab5243befacf8a9fa5034c0e5509d2
ifirmawan <firmawaneiwan@gmail.com>
2024-10-10 20:46:13 +0700

    [#70] Create new test case: users statistics endpoint

---
+++ backend/api/v1/v1_users/tests/tests_users_statistics_endpoint.py
@@ -0,0 +1,60 @@
+from django.test import TestCase
+from django.core.management import call_command
+from django.test.utils import override_settings
+from django.utils import timezone
+from django.db.models import Count
+from datetime import timedelta
+from api.v1.v1_users.models import SystemUser
+from api.v1.v1_users.tests.mixins import ProfileTestHelperMixin
+
+
+@override_settings(USE_TZ=False)
+class UsersStatisticsTestCase(TestCase, ProfileTestHelperMixin):
+    def setUp(self):
+        call_command("fake_users_seeder", "--test", True, "--repeat", 15)
+        self.admin = SystemUser.objects.create_superuser(
+            full_name="Super Admin",
+            email="admin@akvo.org",
+            gender=1,
+            account_purpose=2,
+            country="EN",
+            password="Secret123!",
+        )
+        self.token = self.get_auth_token(
+            email="admin@akvo.org",
+            password="Secret123!",
+        )
+
+    def test_get_users_statistics(self):
+        req = self.client.get(
+            "/api/v1/admin/statistics/users",
+            content_type="application/json",
+            HTTP_AUTHORIZATION=f"Bearer {self.token}",
+        )
+        self.assertEqual(req.status_code, 200)
+        res = req.json()
+        self.assertEqual(res["total_users"], 16)
+        total_users_last_30_days = SystemUser.objects.filter(
+            date_joined__gte=timezone.now() - timedelta(days=30)
+        ).count()
+        self.assertEqual(
+            res["total_users_last_30_days"],
+            total_users_last_30_days
+        )
+
+        total_users_per_account_purpose = SystemUser.objects.values(
+            "account_purpose"
+        ).annotate(
+            total=Count("account_purpose")
+        )
+        account_purpose_total = []
+        for total in total_users_per_account_purpose:
+            account_purpose_total.append({
+                "account_purpose": total["account_purpose"],
+                "total": total["total"]
+            })
+
+        self.assertEqual(
+            res["total_users_per_account_purpose"],
+            account_purpose_total
+        )
---
+++ frontend/src/components/Charts/AccountPurposeChart.js
@@ -0,0 +1,9 @@
+"use client";
+
+import { Doughnut } from "akvo-charts";
+
+const AccountPurposeChart = ({ config, data }) => {
+  return <Doughnut config={config} data={data} />;
+};
+
+export default AccountPurposeChart;
