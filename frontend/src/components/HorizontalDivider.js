d59418c3388a0e7b587afd7f8bda0436c33c5c77
ifirmawan <firmawaneiwan@gmail.com>
2024-10-10 11:31:10 +0700

    [#68] Update HorizontalDivider styles

--- frontend/src/components/HorizontalDivider.js
+++ frontend/src/components/HorizontalDivider.js
@@ -1,6 +1,6 @@
 const HorizontalDivider = ({ children }) => (
   <div className="w-fit py-2">
-    <div className="flex divide-x-2 divide-solid">{children}</div>
+    <div className="flex flex-row items-center divide-x-2 divide-solid">{children}</div>
   </div>
 );
 
