364561590eb9137e164fc69ebe1a23e0c9121354
ifirmawan <firmawaneiwan@gmail.com>
2024-10-10 11:38:12 +0700

    [#68] Update WithNavbar template styles to make navbar sticky top

--- frontend/src/app/[locale]/(with-navbar)/template.js
+++ frontend/src/app/[locale]/(with-navbar)/template.js
@@ -4,9 +4,9 @@ import { Link } from "@/routing";
 
 const WithNavbarTemplate = ({ children }) => {
   return (
-    <div className="w-full max-w-9xl h-screen relative bg-grey-100 text-base text-dark-10 overflow-x-hidden overflow-y-auto 2xl:overflow-y-hidden">
+    <div className="w-full max-w-9xl h-screen relative bg-grey-100 text-base text-dark-10 overflow-x-hidden overflow-y-auto">
       <div className="w-1/2 lg:w-2/5 h-screen absolute bottom-0 right-0 bg-dashboard bg-no-repeat bg-contain bg-right-bottom" />
-      <div className="w-full flex items-center bg-light-1 h-24">
+      <div className="w-full flex items-center bg-light-1 h-24 sticky top-0 z-10">
         <div className="container mx-auto">
           <Flex className="w-full" justify="space-between" align="center">
             <Link href="/dashboard">
