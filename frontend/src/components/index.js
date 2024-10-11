66c78fc89300291f3c40aa8437d2773c1a99347c
ifirmawan <firmawaneiwan@gmail.com>
2024-10-11 10:44:26 +0700

    [#70] Create a new component: AccountPurposeChart

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,6 +1,7 @@
 export * from "./PasswordInput";
 export * from "./Buttons";
 export * from "./Modals";
+export * from "./Charts";
 export { default as RegisterForm } from "./Forms/RegisterForm";
 export { default as LoginForm } from "./Forms/LoginForm";
 export { default as ForgotPasswordForm } from "./Forms/ForgotPasswordForm";
