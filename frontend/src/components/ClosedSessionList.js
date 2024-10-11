b6ddb70bf73124b40b1424e158dbcf3aa7f2abd2
ifirmawan <firmawaneiwan@gmail.com>
2024-10-11 20:21:10 +0700

    [#73] Finalizing backend integration filtering in ClosedSessionList

--- frontend/src/components/ClosedSessionList.js
+++ frontend/src/components/ClosedSessionList.js
@@ -1,6 +1,16 @@
 "use client";
 
-import { Button, Card, Flex, List, Typography, Dropdown } from "antd";
+import {
+  Button,
+  Card,
+  Flex,
+  List,
+  Typography,
+  Dropdown,
+  Form,
+  Input,
+  Select,
+} from "antd";
 import {
   DotsTreeVerticalIcon,
   DownloadSimpleIcon,
@@ -8,13 +18,15 @@ import {
   Eye,
 } from "./Icons";
 import { useTranslations } from "next-intl";
-import { PAT_SESSION } from "@/static/config";
+import { FILTER_BY_ROLE, PAT_SESSION } from "@/static/config";
 import { useRouter } from "@/routing";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { api } from "@/lib";
 import { DeleteSessionModal } from "./Modals";
+import { useSessionDispatch } from "@/context/SessionContextProvider";
 
 const { Title, Text } = Typography;
+const { Search } = Input;
 
 const ClosedSessionList = ({ data = [], totalClosed = 0 }) => {
   const [dataSource, setDataSource] = useState(data);
@@ -22,6 +34,10 @@ const ClosedSessionList = ({ data = [], totalClosed = 0 }) => {
   const [totalData, setTotalData] = useState(totalClosed);
   const [loading, setLoading] = useState(false);
   const [patSession, setPatSession] = useState(null);
+  const [preload, setPreload] = useState(true);
+  const sessionDispatch = useSessionDispatch();
+
+  const [form] = Form.useForm();
 
   const onPaginationChange = async (page) => {
     setLoading(true);
@@ -38,18 +54,87 @@ const ClosedSessionList = ({ data = [], totalClosed = 0 }) => {
     }
   };
 
+  const onFinish = async ({ search, role }) => {
+    setLoading(true);
+    try {
+      const queryParams = [];
+      if (search) queryParams.push(`search=${search}`);
+      if (role) queryParams.push(`role=${role}`);
+      const { data: apiData, total } = await api(
+        "GET",
+        `/sessions?published=true&page=${1}&page_size=${PAT_SESSION.pageSize}&${queryParams.join("&")}`,
+      );
+      setDataSource(apiData);
+      setTotalData(total);
+      sessionDispatch({
+        type: "FILTER_SUBMITTED",
+      });
+      sessionDispatch({
+        type: "TOTAL_CLOSED_UPDATE",
+        payload: total,
+      });
+    } catch (err) {
+      console.error(err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   const onDeleteSession = (id) => {
     const updatedDataSource = dataSource.filter((item) => item.id !== id);
     setDataSource(updatedDataSource);
   };
 
+  useEffect(() => {
+    if (preload) {
+      setPreload(false);
+      sessionDispatch({
+        type: "TOTAL_CLOSED_UPDATE",
+        payload: totalData,
+      });
+    }
+  }, [totalData, preload, sessionDispatch]);
+
   const t = useTranslations("Dashboard");
+  const tc = useTranslations("common");
   const router = useRouter();
 
   return (
-    <div className="w-full pt-2">
+    <div className="w-full pt-2 space-y-2">
       <h2 className="font-bold text-xl">{t("closedSessions")}</h2>
-      <br />
+      <Form form={form} onFinish={onFinish}>
+        <div className="w-full flex flex-row gap-2 items-center justify-between">
+          <div className="w-1/2 lg:w-4/12">
+            <Form.Item name="search">
+              <Search
+                placeholder={tc("search")}
+                onSearch={(value) => {
+                  form.setFieldValue("search", value);
+                  form.submit();
+                }}
+                allowClear
+              />
+            </Form.Item>
+          </div>
+          <div className="w-1/2 lg:w-4/12 2xl:w-3/12">
+            <Form.Item name="role">
+              <Select
+                placeholder={t("filterByRole")}
+                options={Object.keys(FILTER_BY_ROLE).map((role) => ({
+                  label: t(role),
+                  value: FILTER_BY_ROLE[role],
+                }))}
+                className="pat-filter-by-role"
+                onChange={(value) => {
+                  form.setFieldValue("role", value);
+                  form.submit();
+                }}
+                allowClear
+              />
+            </Form.Item>
+          </div>
+        </div>
+      </Form>
       <List
         pagination={
           totalData > PAT_SESSION.pageSize
@@ -67,19 +152,20 @@ const ClosedSessionList = ({ data = [], totalClosed = 0 }) => {
         loading={loading}
         renderItem={(item) => (
           <div className="mb-3">
-            <Card
-              onClick={() => {
-                router.push(`/dashboard/overview/${item?.id}`);
-              }}
-              hoverable
-            >
+            <Card hoverable>
               <Flex align="center" justify="space-between">
-                <div>
+                <div
+                  role="button"
+                  onClick={() => {
+                    router.push(`/dashboard/overview/${item?.id}`);
+                  }}
+                >
                   <Title level={4}>{item?.session_name}</Title>
                   <Text className="line-clamp-1">{item?.context}</Text>
                 </div>
                 <div>
                   <Dropdown
+                    trigger={["click"]}
                     menu={{
                       items: [
                         {
