af97e0e0d457b2e0c3bb1c48ce69c16b65619ed7
ifirmawan <firmawaneiwan@gmail.com>
2024-10-11 20:16:16 +0700

    [#73] Create a new component: TotalClosedLabel
    Get totalClosed from global states SessionContext

---
+++ frontend/src/components/TotalClosedLabel.js
@@ -0,0 +1,28 @@
+"use client";
+
+import { useSessionContext } from "@/context/SessionContextProvider";
+import { Space } from "antd";
+import { useTranslations } from "next-intl";
+import { useMemo } from "react";
+
+export const TotalClosedLabel = ({ initialTotalClosed = 0 }) => {
+  const t = useTranslations("Dashboard");
+
+  const sessionContext = useSessionContext();
+  const { totalClosed, filterSubmitted } = sessionContext;
+
+  const total = useMemo(() => {
+    return filterSubmitted ? totalClosed : totalClosed || initialTotalClosed;
+  }, [totalClosed, filterSubmitted, initialTotalClosed]);
+
+  return (
+    <Space>
+      <span>{t("closedSessions")}</span>
+      <span className="badge-number py-px px-2 border border-dark-2 rounded-full">
+        {total}
+      </span>
+    </Space>
+  );
+};
+
+export default TotalClosedLabel;
