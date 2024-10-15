fbcd30321c22a2189b16fb787bf88a22c3ace382
ifirmawan <firmawaneiwan@gmail.com>
2024-10-15 11:38:27 +0700

    [#77] Handle searchParams question in FAQs page

--- frontend/src/app/[locale]/dashboard/(with-header)/faqs/page.js
+++ frontend/src/app/[locale]/dashboard/(with-header)/faqs/page.js
@@ -3,11 +3,13 @@ import classNames from "classnames";
 import { openSans } from "@/app/fonts";
 import { FAQCollapsible } from "@/components";
 
-const FAQPage = () => {
+const FAQPage = ({ searchParams }) => {
+  const question = searchParams.question;
   return (
     <Card>
       <div className={classNames(openSans.className, "p-12")}>
         <FAQCollapsible
+          defaultActiveKey={[question]}
           wrapClass="w-full flex flex-col items-start justify-start"
           contentClass="w-full 2xl:w-10/12"
           center={false}
