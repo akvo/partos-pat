a40744ded92a33c066b319ddeba43973a461ea9c
ifirmawan <firmawaneiwan@gmail.com>
2024-10-31 15:52:35 +0700

    [#99] Finetune report text

--- frontend/src/components/PrintDocument/PrintPage.js
+++ frontend/src/components/PrintDocument/PrintPage.js
@@ -3,10 +3,11 @@
 import Image from "next/image";
 import moment from "moment";
 import PrintTable from "./PrintTable";
+import ScoreLegend from "../SessionWizard/ScoreLegend";
 
 const style = {
   container: {
-    fontFamily: "Arial, sans-serif",
+    fontFamily: "Open Sans, sans-serif",
     background: "#fff",
     color: "#000",
   },
@@ -100,7 +101,9 @@ const PrintPage = ({
           })}
         </tbody>
       </PrintTable>
-      <p>PAT session context</p>
+      <div style={{ paddingTop: 36, paddingBottom: 16 }}>
+        <strong>PAT session context</strong>
+      </div>
       <PrintTable>
         <tbody>
           <tr>
@@ -108,10 +111,17 @@ const PrintPage = ({
           </tr>
         </tbody>
       </PrintTable>
+      <div style={{ breakAfter: "page" }}></div>
       <h3 style={style.title}>PAT session content</h3>
+      <strong>PAT session Step 1 List important decisions</strong>
       <p>
-        List of important (types of) decisions that were reflected on in this
-        PAT session.
+        As a first step in this joint analysis, each participant was asked to
+        come up with at least one important decision taken in the partnership in
+        the past, that they would like to be reflected on in the PAT session. If
+        two or more participants came up with very similar types of decisions,
+        the facilitator clustered them. This resulted in the following list of
+        important (types of) decisions that were reflected on in this PAT
+        session.
       </p>
       <PrintTable>
         <tbody>
@@ -124,12 +134,25 @@ const PrintPage = ({
           })}
         </tbody>
       </PrintTable>
-      <div style={{ breakAfter: "page" }}></div>
+      <br />
+      <div style={{ paddingTop: 24 }}>
+        <strong>
+          PAT session Step 2 Determine actual level of participation in decision
+          making
+        </strong>
+      </div>
+      <p>
+        Subsequently, for each listed decision, the actual level of
+        participation in taking this decision was assessed and scored for each
+        partner organisation. For this the ladder of participation in
+        decision-making was used. Depending on the level of participation, the
+        score ranged between 0 (not involved) and 4 (partner decides).
+      </p>
+      <ScoreLegend isPrint={true} />
+
       {groupedOrgPer3?.map((organizations, index) => (
         <div key={index}>
-          <p>
-            {`On the following decisions there was agreement on the way partner organizations (PO) are/have been involved in the decision-making process.`}
-          </p>
+          <p>This exercise resulted in the following overview:</p>
           <PrintTable>
             <thead>
               <tr>
@@ -142,36 +165,92 @@ const PrintPage = ({
               </tr>
             </thead>
             <tbody>
-              {decisions
-                ?.filter((d) => d?.agree)
-                ?.map((d) => {
-                  return (
-                    <tr key={d?.id}>
-                      <PrintTable.TD>{d?.name}</PrintTable.TD>
-                      {organizations?.map((o) => {
-                        const actualValue = d?.scores?.find(
-                          (s) => s?.organization_id === o?.id,
-                        );
-                        return (
-                          <PrintTable.TD key={`${d?.id}-${o?.id}`}>
-                            {actualValue?.score}
-                          </PrintTable.TD>
-                        );
-                      })}
-                    </tr>
-                  );
-                })}
+              {decisions?.map((d) => {
+                return (
+                  <tr key={d?.id}>
+                    <PrintTable.TD>{d?.name}</PrintTable.TD>
+                    {organizations?.map((o) => {
+                      const actualValue = d?.scores?.find(
+                        (s) =>
+                          s?.organization_id === o?.id &&
+                          (s?.desired === null || s?.desired === false),
+                      );
+                      return (
+                        <PrintTable.TD key={`${d?.id}-${o?.id}`}>
+                          {actualValue?.score}
+                        </PrintTable.TD>
+                      );
+                    })}
+                  </tr>
+                );
+              })}
             </tbody>
           </PrintTable>
-          <div style={{ breakAfter: "page" }}></div>
         </div>
       ))}
+      <div style={{ paddingTop: 24 }}>
+        <strong>
+          PAT session Step 3 Reflect actual level of participation in decision
+          making
+        </strong>
+      </div>
       {groupedOrgPer3?.map((organizations, index) => (
         <div key={index}>
           <p>
-            On the following decisions there was <b>no agreement</b> on the way
-            partner organizations PO) are/have been involved in the
-            decision-making process.
+            As part of step 3 participants jointly reflected on the levels of
+            participation. They discussed whether there was consensus in the way
+            these decisions were being taken in the partnership, or not. The
+            table below depicts the same table as in step 2, but this table has
+            one additional column at the right, stating whether there was
+            agreement, or no agreement with the way these decisions were being
+            taken.
+          </p>
+          <PrintTable>
+            <thead>
+              <tr>
+                <PrintTable.TH>(Types of) Decisions</PrintTable.TH>
+                {organizations?.map((o) => {
+                  return (
+                    <PrintTable.TH key={o?.id}>{o?.acronym}</PrintTable.TH>
+                  );
+                })}
+                <PrintTable.TH>Agree?</PrintTable.TH>
+              </tr>
+            </thead>
+            <tbody>
+              {decisions?.map((d) => {
+                return (
+                  <tr key={d?.id}>
+                    <PrintTable.TD>{d?.name}</PrintTable.TD>
+                    {organizations?.map((o) => {
+                      const actualValue = d?.scores?.find(
+                        (s) =>
+                          s?.organization_id === o?.id &&
+                          (s?.desired === null || s?.desired === false),
+                      );
+                      return (
+                        <PrintTable.TD key={`${d?.id}-${o?.id}`}>
+                          {actualValue?.score}
+                        </PrintTable.TD>
+                      );
+                    })}
+                    <PrintTable.TD>{d?.agree ? "Yes" : "No"}</PrintTable.TD>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </PrintTable>
+        </div>
+      ))}
+
+      {groupedOrgPer3?.map((organizations, index) => (
+        <div key={index}>
+          <p>
+            From the above overview it can be concluded that with regard to the
+            following decisions there was no agreement on the way partner
+            organizations (PO) are/have been involved in the decision-making
+            process. This table was the starting point of step 4 of the PAT
+            session.
           </p>
           <PrintTable>
             <thead>
@@ -195,7 +274,7 @@ const PrintPage = ({
                         const actualValue = d?.scores?.find(
                           (s) =>
                             s?.organization_id === o?.id &&
-                            s?.desired === false,
+                            (s?.desired === null || s?.desired === false),
                         );
                         return (
                           <PrintTable.TD key={`${d?.id}-${o?.id}`}>
@@ -208,29 +287,22 @@ const PrintPage = ({
                 })}
             </tbody>
           </PrintTable>
-          <div style={{ breakAfter: "page" }}></div>
         </div>
       ))}
-      <br />
-      {patSession?.notes?.length > 0 && (
-        <div>
-          <p>
-            Notes on decision-making processes on which there was no agreement.
-          </p>
-          <PrintTable>
-            <tbody>
-              <tr>
-                <PrintTable.TD>{patSession.notes}</PrintTable.TD>
-              </tr>
-            </tbody>
-          </PrintTable>
-        </div>
-      )}
-      <br />
+      <div style={{ paddingTop: 24 }}>
+        <strong>
+          PAT session Step 4 Determine level of participation in decision making
+        </strong>
+      </div>
       {groupedOrgPer3?.map((organizations, index) => (
         <div key={index}>
           <p>
-            {`After discussion, participants agreed that in these decisions partner organisations need to be involved in a different way. The desired level of participation for partner organisations is indicated in the table below:`}
+            With regard to all the decisions on which there was no agreement
+            about the actual level of participation, participants had a
+            discussion in which they tried to find a desired level of
+            participation on which all partners could agree. The desired level
+            of participation for partner organisations for all these decisions
+            is indicated in the table below:
           </p>
           <PrintTable>
             <thead>
@@ -244,38 +316,67 @@ const PrintPage = ({
               </tr>
             </thead>
             <tbody>
-              {decisions?.map((d) => {
-                return (
-                  <tr key={d?.id}>
-                    <PrintTable.TD>{d?.name}</PrintTable.TD>
-                    {organizations?.map((o) => {
-                      const actualValue = d?.scores?.find(
-                        (s) =>
-                          s?.organization_id === o?.id &&
-                          (s?.desired === true || s?.desired === null),
-                      );
-                      return (
-                        <PrintTable.TD key={`${d?.id}-${o?.id}`}>
-                          {actualValue?.score}
-                        </PrintTable.TD>
-                      );
-                    })}
-                  </tr>
-                );
-              })}
+              {decisions
+                ?.filter((d) => !d?.agree)
+                ?.map((d) => {
+                  return (
+                    <tr key={d?.id}>
+                      <PrintTable.TD>{d?.name}</PrintTable.TD>
+                      {organizations?.map((o) => {
+                        const actualValue = d?.scores?.find(
+                          (s) => s?.organization_id === o?.id && s?.desired,
+                        );
+                        return (
+                          <PrintTable.TD key={`${d?.id}-${o?.id}`}>
+                            {actualValue?.score}
+                          </PrintTable.TD>
+                        );
+                      })}
+                    </tr>
+                  );
+                })}
             </tbody>
           </PrintTable>
-          <div style={{ breakAfter: "page" }}></div>
         </div>
       ))}
+      <br />
+
+      <div>
+        <p>
+          Notes on decision-making processes on which there was no agreement.
+        </p>
+        <PrintTable>
+          <tbody>
+            <tr>
+              <PrintTable.TD>
+                <em>{patSession.notes}</em>
+              </PrintTable.TD>
+            </tr>
+          </tbody>
+        </PrintTable>
+      </div>
+      <br />
+      <div style={{ paddingTop: 24 }}>
+        <strong>
+          PAT session Step 5 Determine level of participation in decision making
+        </strong>
+      </div>
+
       <p>
-        {`In order to achieve the desired level of participation of all partner organisations, the participants agree that the following needs to be done:`}
+        In the fifth and final step of the PAT exercise participants determined
+        what action needs to be taken in order to get to the desired level of
+        participation. Of course, this is only applicable to types of decision
+        making where the desired level of participation differed from the actual
+        level of participation.
       </p>
       <PrintTable>
         <thead>
           <tr>
-            <PrintTable.TH>(Types of) Decisions</PrintTable.TH>
-            <PrintTable.TH>{`Action to be taken in order to achieve that partner organization can participate as desired.`}</PrintTable.TH>
+            <PrintTable.TH width="50%">(Types of) Decisions</PrintTable.TH>
+            <PrintTable.TH>
+              Action to be taken in order to achieve that partner organization
+              can participate as desired.
+            </PrintTable.TH>
           </tr>
         </thead>
         <tbody>
@@ -291,41 +392,50 @@ const PrintPage = ({
             })}
         </tbody>
       </PrintTable>
-      {allComments?.length > 0 && <p>Participant Feedback Summary</p>}
-      <ul style={{ marginLeft: "-24px" }}>
-        {allComments?.map((c, cx) => (
-          <li key={cx}>
-            <div style={{ padding: "8px 0", fontSize: 14 }}>
-              <strong>{c?.organization_name}</strong>
-            </div>
-            <PrintTable>
-              <tbody>
-                {c?.comments?.map((comment, index) => (
-                  <tr key={index}>
-                    <PrintTable.TD>
-                      <strong>{comment?.fullname}</strong>
-                      <br />
-                      <em>{comment?.comment}</em>
-                    </PrintTable.TD>
-                  </tr>
-                ))}
-              </tbody>
-            </PrintTable>
-          </li>
-        ))}
-      </ul>
-      {patSession?.summary?.length > 0 && (
-        <div>
-          <p>Concluding remarks</p>
-          <PrintTable>
-            <tbody>
-              <tr>
-                <PrintTable.TD>{patSession.summary}</PrintTable.TD>
+      <p>Participant Feedback Summary</p>
+      <PrintTable>
+        <tbody>
+          {allComments.length ? (
+            allComments?.map((c, cx) => (
+              <tr key={cx}>
+                <PrintTable.TD>
+                  <div style={{ padding: "8px 0", fontSize: 14 }}>
+                    <strong>{c?.organization_name}</strong>
+                  </div>
+                  <PrintTable>
+                    <tbody>
+                      {c?.comments?.map((comment, index) => (
+                        <tr key={index}>
+                          <PrintTable.TD>
+                            <strong>{comment?.fullname}</strong>
+                            <br />
+                            <em>{comment?.comment}</em>
+                          </PrintTable.TD>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </PrintTable>
+                </PrintTable.TD>
               </tr>
-            </tbody>
-          </PrintTable>
-        </div>
-      )}
+            ))
+          ) : (
+            <tr>
+              <PrintTable.TD />
+            </tr>
+          )}
+        </tbody>
+      </PrintTable>
+
+      <div>
+        <p>Concluding remarks</p>
+        <PrintTable>
+          <tbody>
+            <tr>
+              <PrintTable.TD>{patSession.summary}</PrintTable.TD>
+            </tr>
+          </tbody>
+        </PrintTable>
+      </div>
     </div>
   );
 };
