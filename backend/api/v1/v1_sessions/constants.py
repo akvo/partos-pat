8dd150942d27cc5378232809710b6e7d93e3dd79
ifirmawan <firmawaneiwan@gmail.com>
2024-11-12 21:08:21 +0700

    [#113] Rename sector to purpose & generate modify migration

--- backend/api/v1/v1_sessions/constants.py
+++ backend/api/v1/v1_sessions/constants.py
@@ -1,28 +1,28 @@
-class SectorTypes:
-    sector_other = 0
-    sector1 = 1
-    sector2 = 2
-    sector3 = 3
-    sector4 = 4
-    sector5 = 5
-    sector6 = 6
-    sector7 = 7
-    sector8 = 8
-    sector9 = 9
-    sector10 = 10
+class SessionPurpose:
+    other_purpose = 0
+    purpose1 = 1
+    purpose2 = 2
+    purpose3 = 3
+    purpose4 = 4
+    purpose5 = 5
+    purpose6 = 6
+    purpose7 = 7
+    purpose8 = 8
+    purpose9 = 9
+    purpose10 = 10
 
     FieldStr = {
-        sector1: "Policy coherence",
-        sector2: "International fair agreements",
-        sector3: "Equality, inclusion & diversity",
-        sector4: "Human rights",
-        sector5: "Global Citizen & Global Engagement",
-        sector6: "Collaborate on sustainable development",
-        sector7: "Promoting peace and stabilising conflict",
-        sector8: "Nature, environment & climate justice",
-        sector9: "Fair distribution & access to basic amenities",
-        sector10: "Humanitarian emergency aid",
-        sector_other: "Other"
+        purpose1: "Purpose of creation #1",
+        purpose2: "Purpose of creation #2",
+        purpose3: "Purpose of creation #3",
+        purpose4: "Purpose of creation #4",
+        purpose5: "Purpose of creation #5",
+        purpose6: "Purpose of creation #6",
+        purpose7: "Purpose of creation #7",
+        purpose8: "Purpose of creation #8",
+        purpose9: "Purpose of creation #9",
+        purpose10: "Purpose of creation #10",
+        other_purpose: "Other"
     }
 
 
