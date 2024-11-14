9bb2515343b612987da7eb4d2336fca4b6ffe87f
ifirmawan <firmawaneiwan@gmail.com>
2024-11-14 20:01:46 +0700

    [#104] Add z-index in with-header template

--- frontend/src/app/[locale]/dashboard/(with-header)/template.js
+++ frontend/src/app/[locale]/dashboard/(with-header)/template.js
@@ -3,7 +3,7 @@ import { BreadCrumb } from "@/components";
 const WithHeaderTemplate = ({ children }) => {
   return (
     <>
-      <div className="w-full h-44 flex flex-col justify-center bg-dashboard-header-gradient px-10">
+      <div className="w-full h-44 flex flex-col justify-center bg-dashboard-header-gradient px-10 relative z-20">
         <div className="w-[420px] h-44 absolute top-0 left-[280px] bg-dashboard-circle bg-no-repeat bg-cover bg-center" />
         <BreadCrumb />
       </div>
